985085b765de0f12d6db9375302b93b5a41cf625
ErikWDev <ErikWDev@gmail.com>
2023-10-11 14:52:57 +0200

    Almost game

--- src/EDAF80/assignment5.cpp
+++ src/EDAF80/assignment5.cpp
@@ -46,7 +46,7 @@ typedef struct sphere_t {
 } sphere_t;
 
 
-bool sphere_v_sphere_check(sphere_t a, sphere_t b) {
+bool sphere_v_sphere(sphere_t a, sphere_t b) {
 	auto distance_squared =  glm::length2(a.point - b.point);
 
 	auto r_tot = a.radius + b.radius;
@@ -141,6 +141,11 @@ edaf80::Assignment5::run()
 	if (shape.vao == 0u)
 		return;
 
+	auto const sphere = parametric_shapes::createSphere(5.0, 10, 10);
+	if (sphere.vao == 0u)
+		return;
+	
+
 	auto const space_ship_mesh = parametric_shapes::createSphere(10.0, 10, 10);
 	if (space_ship_mesh.vao == 0u)
 		return;
@@ -185,8 +190,8 @@ edaf80::Assignment5::run()
 	water_node.set_program(&erik_water_shader, set_uniforms);
 	water_node.add_texture("normal_map", normal_map, GL_TEXTURE_2D);
 	water_node.add_texture("sky_box", cubemap, GL_TEXTURE_CUBE_MAP);
+	water_node.get_transform().SetTranslate(glm::vec3(0.0, 10.0, 0.0));
 	// the_node.get_transform().RotateX(3.141592f);
-
 
 	bonobo::material_data demo_material;
 	demo_material.ambient = glm::vec3(0.1f, 0.1f, 0.1f);
@@ -203,6 +208,52 @@ edaf80::Assignment5::run()
 	space_ship_node.add_texture("diffuse_map", diffuse_map, GL_TEXTURE_2D);
 	space_ship_node.add_texture("rough_map", rough_map, GL_TEXTURE_2D);
 
+	std::vector<Node> Targets_node = std::vector<Node>();
+    std::vector<float> Targets_rad = std::vector<float>();
+    std::vector<float> Targets_t = std::vector<float>();
+    std::vector<float> Targets_y = std::vector<float>();
+    std::vector<float> Targets_speed = std::vector<float>();
+
+	float outRad = 200.0;
+	float inRad = 130.0;
+	float coordSpace = outRad - inRad;
+
+    for (unsigned int i = 0u; i < 15 ; ++i) {
+        int rand_val = rand();
+        double boundary = 4.5;
+        float x_plane = ((rand_val / (RAND_MAX * 1.0f)) * boundary) + inRad; // random x-value from 0 to 4.5 (outer radius - inner radius)
+        float x_coord = x_plane + (boundary * i);
+        auto the_node = Node();
+
+		auto const phong_set_uniforms = [&use_normal_mapping, &light_position, &camera_position](GLuint program){
+			glUniform1i(glGetUniformLocation(program, "use_normal_mapping"), use_normal_mapping ? 1 : 0);
+			glUniform3fv(glGetUniformLocation(program, "light_position"), 1, glm::value_ptr(light_position));
+			glUniform3fv(glGetUniformLocation(program, "camera_position"), 1, glm::value_ptr(camera_position));
+		};
+		
+        the_node.set_geometry(sphere);
+		the_node.set_program(&erik_phong_shader, phong_set_uniforms);
+		the_node.add_texture("cubemap", cubemap, GL_TEXTURE_CUBE_MAP);
+		the_node.add_texture("normal_map", normal_map, GL_TEXTURE_2D);
+		the_node.add_texture("diffuse_map", diffuse_map, GL_TEXTURE_2D);
+		the_node.add_texture("rough_map", rough_map, GL_TEXTURE_2D);
+    
+        Targets_rad.push_back(x_coord);
+        Targets_node.push_back(the_node);
+
+        rand_val = rand();
+        float y_plane = (rand_val / (RAND_MAX * 1.0f)) * 30.0 + 10.0;
+		Targets_y .push_back(y_plane);
+
+        rand_val = rand();
+        float t = ((rand_val / (RAND_MAX * 1.0f)) * 3.141592);
+		Targets_t.push_back(t);
+
+        rand_val = rand();
+        float speed = ((rand_val / (RAND_MAX * 1.0f)) * 0.4);
+		Targets_speed.push_back(t);
+    }
+
 	glClearDepthf(1.0f);
 	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
@@ -213,7 +264,7 @@ edaf80::Assignment5::run()
 	bool use_orbit_camera = false;
 	auto cull_mode = bonobo::cull_mode_t::disabled;
 	auto polygon_mode = bonobo::polygon_mode_t::fill;
-	bool show_logs = true;
+	bool show_logs = false;
 	bool show_gui = true;
 	bool shader_reload_failed = false;
 	bool show_basis = false;
@@ -224,6 +275,8 @@ edaf80::Assignment5::run()
 	float radius = 80.0f;
 	float speed = 0.7f;
 	float t = 0.0;
+
+	int score = 0;
 
 	changeCullMode(cull_mode);
 
@@ -254,9 +307,12 @@ edaf80::Assignment5::run()
 			if ((inputHandler.GetKeycodeState(GLFW_KEY_D) & PRESSED)) movement.x -= 1.0f;
 			if ((inputHandler.GetKeycodeState(GLFW_KEY_Q) & PRESSED)) movement.y -= 1.0f;
 			if ((inputHandler.GetKeycodeState(GLFW_KEY_E) & PRESSED)) movement.y += 1.0f;
+			if ((inputHandler.GetKeycodeState(GLFW_KEY_W) & PRESSED)) speed += 0.1f;
+			if ((inputHandler.GetKeycodeState(GLFW_KEY_S) & PRESSED)) speed -= 0.1f;
 
 			ship_velocity.x += movement.x;
 			ship_velocity.y += movement.y;
+			speed = std::clamp(speed, 0.2f, 2.0f);
 		}
 
 		ship_velocity *= 0.98;
@@ -279,6 +335,46 @@ edaf80::Assignment5::run()
 
 		auto& ship_transform = space_ship_node.get_transform();
 		ship_transform.SetTranslate(new_ship_pos);
+
+		std::vector<int> to_remove;
+
+		sphere_t player_sphere = sphere_t {
+			.radius = 3.0f,
+			.point = new_ship_pos,
+		};
+
+	    for(unsigned int i = 0u; i < Targets_node.size(); ++i){
+	        auto& the_node = Targets_node[i];
+
+			float speed = Targets_speed[i];
+			Targets_t[i] += dt *speed;
+
+			float r = Targets_rad[i]; 
+			float t = Targets_t[i];
+			float y = Targets_y[i];
+
+			auto p = glm::vec3(cos(t), 0.0, sin(t)) * r;
+			auto target_pos = origin + p + glm::vec3(0.0, y, 0.0);
+	        the_node.get_transform().SetTranslate(target_pos);
+
+			sphere_t target_sphere = sphere_t {
+				.radius = 5.0f,
+				.point = target_pos,
+			};
+
+			if (sphere_v_sphere(player_sphere, target_sphere)) {
+				score += 100;
+				to_remove.push_back(i);
+			}
+	     }
+
+		for (int i = 0; i < to_remove.size(); i++) {
+			Targets_node.erase(Targets_node.begin() + i);
+			Targets_rad.erase(Targets_rad.begin() + i);
+			Targets_t.erase(Targets_t.begin() + i);
+			Targets_y.erase(Targets_y.begin() + i);
+			Targets_speed.erase(Targets_speed.begin() + i);
+		}
 
 		// mCamera.mWorld.SetTranslate(glm::vec3(0.0, 30.0, 0.0));
 
@@ -324,12 +420,14 @@ edaf80::Assignment5::run()
 			// Todo: Render all your geometry here.
 			//
 			// mCamera.mWorld.SetTranslate(p);
-			skybox.render(mCamera.GetWorldToClipMatrix());
-			water_node.render(mCamera.GetWorldToClipMatrix());
-
-			space_ship_node.render(mCamera.GetWorldToClipMatrix());
+			auto vp = mCamera.GetWorldToClipMatrix();
+			skybox.render(vp);
+			water_node.render(vp);
+			space_ship_node.render(vp);
+			for (int i = 0; i < Targets_node.size(); i++) {
+				Targets_node[i].render(vp);
+			}
 		}
-
 
 		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
@@ -338,18 +436,20 @@ edaf80::Assignment5::run()
 		//       here
 		//
 
-
-		bool opened = ImGui::Begin("Scene Control", nullptr, ImGuiWindowFlags_None);
+		bool opened = ImGui::Begin("Scene Control", nullptr, ImGuiWindowFlags_None | ImGuiWindowFlags_NoTitleBar);
 		if (opened) {
-			ImGui::Separator();
-			ImGui::Checkbox("Show basis", &show_basis);
+			float s = (50.0f - elapsed_time_s);
+			ImGui::DragInt(" Points", &score, 0.1f, 0, 999999999);
+			ImGui::DragFloat(" s", &s, 0.1f, 0, 99999999.0f);
 		}
 		ImGui::End();
 
 		if (show_basis)
 			bonobo::renderBasis(basis_thickness_scale, basis_length_scale, mCamera.GetWorldToClipMatrix());
+
 		if (show_logs)
 			Log::View::Render();
+
 		mWindowManager.RenderImGuiFrame(show_gui);
 
 		glfwSwapBuffers(window);
